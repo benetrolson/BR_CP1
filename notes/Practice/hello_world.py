ebbc3d9e7b8d587e08548026944f183d078f8dc3
benetrolson <bennett.rolfson@ucas-edu.net>
2025-08-20 10:47:40 -0600

    Hello world

---
+++ notes/Practice/hello_world.py
@@ -0,0 +1,7 @@
+#BHR 2nd hello world
+name = input("Hello random person. I know I shouldn't talk to strangers but what is your name? ")
+print("Why hello", name, ", nice to meet you. ")
+house = input("Where do you live? ")
+print("I know it's not", house, "now tell me where you live! ")
+home = input("Please? ")
+print("Thank you for telling me. I'll be heading to ", home, "immediately. ")
--- notes/test.py
+++ notes/test.py
@@ -1,3 +1,9 @@
+# BHR 2nd Inputs and Outputs notes
 print("Test")
 #print() <- stuff inside the parentheses goes into the terminal
 
+color = input("Give me a color: ")
+print("I hate the color you chose,", color)
+print("Give me a color: ")
+color_two = input()
+print(color_two)
