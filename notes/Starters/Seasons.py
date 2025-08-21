130af20ddff6332058c5c9ae256e51fe31809e82
benetrolson <bennett.rolfson@ucas-edu.net>
2025-08-21 10:11:22 -0600

    Seasons

---
+++ notes/Starters/Seasons.py
@@ -0,0 +1,4 @@
+# BHR 2nd Seasons
+last_name = input("What is your last name? ")
+favorite_season = input("And what is your favorite season? ")
+print("Wow ", last_name, "san, I like ", favorite_season, "too. ")
