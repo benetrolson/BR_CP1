f98ef4e2589fcf7955a2479ca742c212d10e74f8
benetrolson <bennett.rolfson@ucas-edu.net>
2025-08-25 10:26:12 -0600

    if nested in if who are you

--- notes/Projects/who_are_you.py
+++ notes/Projects/who_are_you.py
@@ -1,8 +1,15 @@
 # BHR 2nd Who are you
 name = input("Howdy. My name is George. What's your name? ")
-age = int(input("Nice to meet you. Say, how old are you?(please just answer with a number, not written out)"))
+age = int(input("Nice to meet you. Say, how old are you?(please just answer with a number, not written out) "))
 if age >= 18:
     color = input("You are very old. What is your favorite color? ")
+    if color == "blue" or color == "teal":
+        print("So", name, ",you are super old,", age, "years old, and your favorite color is", color, ". That is an amazing color, by the way. Bye. ")
+    else:
+        print("So", name, ",you are super old,", age, "years old, and your favorite color is", color, ". That is a rather disgusting color, by the way. Bye. ")
 else:
     color = input("You are really young. What is your favorite color? ")
-print("So ", name, ",you are ", age, "years old, and your favorite color is ", color, ". That is a rather disgusting color, by the way. Bye. ")
+    if color == "blue" or color == "teal":
+        print("So", name, ",you are super young", age, "years old, and your favorite color is", color, ". That is an amazing color, by the way. Bye. ")
+    else:
+        print("So", name, ",you are super young", age, "years old, and your favorite color is", color, ". That is a rather disgusting color, by the way. Bye. ")
