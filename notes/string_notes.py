c30a5b6b3e2c4d13f0019bbcd6afcda43b82b3d1
benetrolson <bennett.rolfson@ucas-edu.net>
2025-09-02 10:51:55 -0600

    string

--- notes/string_notes.py
+++ notes/string_notes.py
@@ -1,1 +1,10 @@
 #BHR 2nd string notes
+month = "September"
+bob = len(month)
+print(month, "has\n\t     ", bob, "letters. ")
+print('Will said "Don\'t die." ')
+first_name = "Xavier"
+last_name = "LaRose"
+full_name = first_name + " " + last_name
+print(full_name)
+print(last_name[2:6])
