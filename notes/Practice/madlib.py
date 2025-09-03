2d39da2d76932480a0b7c91f128c2458bbcade85
benetrolson <bennett.rolfson@ucas-edu.net>
2025-09-03 10:51:56 -0600

    madlib

---
+++ notes/Practice/madlib.py
@@ -0,0 +1,17 @@
+#BHR 2nd madlib
+name1 = input("What is your name? ")
+verbing = input("What is a verb ending in ing? ")
+location1 = input("What is a location? ")
+noun1 = input("What is a noun? ")
+name2 = input("What is your friend's name? ")
+location2 = input("What is a location? ")
+favorite_animal = input("What is you favorite animal? ")
+other_animal = (input("What is an animal? "))
+past_verb = (input("What is a past tense verb? "))
+other_other_anmial = (input("What is another animal? "))
+body_part = (input("What is a body part? "))
+proffesion = (input("What is a proffesion? "))
+
+
+full_thing = name1 + " was " + verbing + " down the " + location1 + " and tripped over a " + noun1 + ". " + name1 + " was approached by a stranger who introduced their name as " + name2 + ". They then guided " + name1 + " over to " + location2 + ". You arrived at the zoo that had a " + favorite_animal + " on display. " + name1 + " then resolved themself to free the " + favorite_animal + ". " + name1 + " broke the glass and grabbed " + other_animal + " instead of the " + favorite_animal + ". " + name1 + " threw the " + other_animal + " out in their rush. The " + other_animal + " scurried around, wreaking havoc. It " + past_verb + " the " + other_other_anmial + " and smacked a person in the " + body_part + ". " + name1 + "was then arrested by the " + proffesion + ". "
+print(full_thing)
