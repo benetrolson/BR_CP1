c8c9b98d6f0db1dc00a400e84c5b56e4bb22d016
benetrolson <bennett.rolfson@ucas-edu.net>
2025-09-05 10:51:45 -0600

    idiot proof

--- notes/Practice/idiot_proof.py
+++ notes/Practice/idiot_proof.py
@@ -1,5 +1,7 @@
 #BHR 2nd Idiot Proof
 name = input("What is your name? ").strip().title()
 number = input("What is your number? ").strip()
-GP = input("What is your grade point average? ").strip()
-all_together = (f"Hi, {name}, your number is {}
+grade_average = input("What is your grade point average? ").strip
+print(f"Your name is {name}")
+
+
