e0a949d5e576b723fbd5bd2931ca976412643f12
brolfson <bennett.rolfson@ucas-edu.net>
2025-09-05 18:31:40 -0600

    Update idiot_proof.py

--- notes/Practice/idiot_proof.py
+++ notes/Practice/idiot_proof.py
@@ -1,12 +1,20 @@
 #BHR 2nd Idiot Proof
 name = input("What is your name? ").strip().title()
-number = input("What is your number? ")
-grade_average = float(input("What is your grade point average? "))
+while True:
+    number = input("What is your number? ")
+    if len(number) == 10 and number.isdigit() and not " " in number:
+        break
+    else:
+        print("That is invalid. Please try again. ")
 number1 = number[:3]
 number2 = number[3:6]
 number3 = number[6:]
 correct_number = number1 + " " + number2 + " " + number3
+while True:
+    grade_average = float(input("What is your grade point average? "))
+    if grade_average >= 4:
+	    print("That is an invalid GPA. Please try again. ")
+    else:
+	    break
 correct_average = round(grade_average, 1)
-if grade_average >= 4:
-	grade_average = 4.0
 print(f"Your name is {name}, your number is {correct_number}, and your grade average is {correct_average}. ")
