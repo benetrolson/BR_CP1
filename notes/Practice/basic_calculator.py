2029e5a647c2a3c5e5b0fa8f5980a15f7b14fd7e
brolfson <bennett.rolfson@ucas-edu.net>
2025-09-08 12:33:12 -0600

    Update basic_calculator.py

--- notes/Practice/basic_calculator.py
+++ notes/Practice/basic_calculator.py
@@ -8,4 +8,4 @@ multiplaction = number1 * number2
 division = number1 / number2
 modulo = number1 % number2
 int_division = number1 // number2
-print(f"{number1} plus {number2} is {addition}. \n{number1} minus {number2} is {subtraction}. \n{number1} times {number2} is {multiplaction}. \n{number1} divided by {number2} is {division}. \nThe remainder of {number1} divided by {number2} is {modulo}. \n{number1} divided by {number2} with no remainder or decimal is {int_division}. \n{number1} to the power of {number2} is {exponent}. ")
+print(f"{number1} + {number2} = {addition}\n{number1} - {number2} = {subtraction}\n{number1}*{number2} = {multiplaction}\n{number1}/{number2} = {division}\nThe remainder of {number1}/{number2} = {modulo}\nWith no remainder, {number1}/{number2} = {int_division}\n{number1}^{number2} = {exponent}")
