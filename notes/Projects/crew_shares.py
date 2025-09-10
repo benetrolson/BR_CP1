6418fd36b76075420c510e80732b79af782213d1
brolfson <bennett.rolfson@ucas-edu.net>
2025-09-10 12:33:19 -0600

    Update crew_shares.py

--- notes/Projects/crew_shares.py
+++ notes/Projects/crew_shares.py
@@ -1,9 +1,13 @@
 # BHR 2nd Crew Shares
 
-earnings = int(input("How many dollars did Bob(the ship's captain) and his crew earn? "))
+earnings = float(input("How many dollars did Bob(the ship's captain) and his crew earn? "))
 crew = int(input("How many people are in the crew? "))
 bass_share = earnings/(crew + 10)
 share_for_mate = bass_share * 3
 share_for_bob = bass_share * 7
 share_per_crew = bass_share - 500
-print(f"Bob(the captain) earned ${share_for_bob}. \nGeorge(the first mate) earned ${share_for_mate}. \nAnd the crew members each earned an additional ${share_per_crew}. \nThe bass share is ${bass_share}. ")
+if share_per_crew < 0:
+  share_per_crew = share_per_crew * (-1)
+  print(f"Bob(the captain) earned ${share_for_bob}. \nGeorge(the first mate) earned ${share_for_mate}. \nAnd the crew members each have to pay their captain ${share_per_crew}. \nThe bass share is ${bass_share}. ")
+else:
+    print(f"Bob(the captain) earned ${share_for_bob}. \nGeorge(the first mate) earned ${share_for_mate}. \nAnd the crew members each earned an additional ${share_per_crew}. \nThe bass share is ${bass_share}. ")
