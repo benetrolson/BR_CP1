9d5b8c79151368a2a298834aca707a7a754dbbbb
benetrolson <bennett.rolfson@ucas-edu.net>
2025-09-15 10:42:32 -0600

    Actualy finished crew shares

--- notes/Projects/crew_shares.py
+++ notes/Projects/crew_shares.py
@@ -1,7 +1,7 @@
 # BHR 2nd Crew Shares
 earnings = float(input("How many dollars did Bob(the ship's captain) and his crew earn? "))
 while True:
-    crew = int(input("How many people are in the crew? "))
+    crew = float(input("How many people are in the crew? "))
     if crew < 0.1:
         print("Please try again. The crew cannot be that small. ")
     else:
@@ -12,6 +12,6 @@ share_for_bob = bass_share * 7
 share_per_crew = bass_share - 500
 if share_per_crew < 0:
   share_per_crew = share_per_crew * (-1)
-  print(f"Bob(the captain) earned ${share_for_bob}. \nGeorge(the first mate) earned ${share_for_mate}. \nAnd the crew members each have to pay their captain ${share_per_crew}. \nThe bass share is ${bass_share}. ")
+  print(f"Bob(the captain) earned ${share_for_bob:.2f}. \nGeorge(the first mate) earned ${share_for_mate:.2f}. \nAnd the crew members each have to pay their captain ${share_per_crew:.2f}. \nThe bass share is ${bass_share:.2f}. ")
 else:
-    print(f"Bob(the captain) earned ${share_for_bob}. \nGeorge(the first mate) earned ${share_for_mate}. \nAnd the crew members each earned an additional ${share_per_crew}. \nThe bass share is ${bass_share}. ")
+    print(f"Bob(the captain) earned ${share_for_bob:.2f}. \nGeorge(the first mate) earned ${share_for_mate:.2f}. \nAnd the crew members each earned an additional ${share_per_crew:.2f}. \nThe bass share is ${bass_share:.2f}. ")
---
+++ notes/Projects/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+{share_per_crew:.2f}. \nThe bass share is ${bass_share:.2f}. ")
+else:
