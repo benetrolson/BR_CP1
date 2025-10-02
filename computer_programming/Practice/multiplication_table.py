2d44c09e39d0558bf2b5dd19ab7af870f9f6988d
benetrolson <bennett.rolfson@ucas-edu.net>
2025-10-02 10:32:46 -0600

    table

--- computer_programming/Practice/multiplication_table.py
+++ computer_programming/Practice/multiplication_table.py
@@ -1,6 +1,11 @@
 #BHR 2nd multiplication table
-
+import time
+delay = 0.01
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-print(f"x |  1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12")
+print(f"x | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12")
 for num in nums:
-    print(f"{num*1} |  {num*1} | {num*2} | {num*3} | {num*4} | {num*5} | {num*6} | {num*7} | {num*8} | {num*9} | {num*10} | {num*11} | {num*12}")
+    time.sleep(0.3)
+    chart = f"{num*1} | {num*1} | {num*2} | {num*3} | {num*4} | {num*5} | {num*6} | {num*7} | {num*8} | {num*9} | {num*10} | {num*11} | {num*12}\n"
+    for char in chart:
+        print(char, end="", flush = True)
+        time.sleep(delay)
