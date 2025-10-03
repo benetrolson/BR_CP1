b25b83f5d712ba4fe395cc5b0c8e3099696936e9
brolfson <bennett.rolfson@ucas-edu.net>
2025-10-03 15:39:34 -0600

    Update rock_paper_scissors.py

--- computer_programming/Practice/rock_paper_scissors.py
+++ computer_programming/Practice/rock_paper_scissors.py
@@ -5,7 +5,7 @@ import random
 print("Welcome to rock paper scissors. ")
 while True:
     choice = input("Which do you want to choose, rock(r), paper(p), or scissors(s)? If you want to quit, then write 'q'. ")
-    if choice == "w":
+    if choice == "q":
         break
     elif choice == "r":
         choose = 1
@@ -13,4 +13,8 @@ while True:
         choose = 2
     elif choice == "s":
         choose = 3
+robo_choice = random.randint(1,3)
 
+if choose == 1 and robot_choice == 2 or choose == 2 and robot_choice == 3 or choose == 3 and robot_choice == 1:
+    print("You won! ")
+elif choose == 1 and robot_choice == 1 or choose == 2 and robot_choice == 2 or choose == 3 and robot_choice == 3:
