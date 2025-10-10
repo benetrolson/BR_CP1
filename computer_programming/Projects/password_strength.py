9da8ce0f2cadb7fee673239a296e01f53c6ae9d9
brolfson <bennett.rolfson@ucas-edu.net>
2025-10-10 15:26:25 -0600

    Update password_strength.py

--- computer_programming/Projects/password_strength.py
+++ computer_programming/Projects/password_strength.py
@@ -17,47 +17,48 @@ while True:
         #add 1 to the password strength
         strength += 1
     #otherwise
-
+    else:
         #say that they need to make their password longer
-
+        print("You could make your password longer. ")
     #looping through every single letter
-
+    for letter in password:
         #check if that letter is uppercase
-
+        if letter.isupper()
             #add 1 to the password strength
-            
+            strength += 1
+            break
         #otherwise
-
+        else:
             #say that they have to add an uppercase letter
-
+            print("You should add an uppercase letter. ")
     #looping through every single letter
-
+    for letter in password:
         #check if that letter is lowercase
-
+        if letter.islower()
             #add 1 to the password strength
-
+            strength += 1
         #otherwise
-
+        else:
             #say that they have to add an lowercase letter
-
+            print("You should add a lowercase letter. ")
     #check if the string is not alpha numeric (isalnum)
-
+    if password not.isalnum
         #add 1 to the password strength
-
+        strength += 1
     #otherwise
-
+    else:
         #say that they need to add a special character
-
+        print("You should add a special character. ")
     #looping through every single letter
-
+    for letter in password
         #check if that letter is numeric
-
+        if letter.isnum()
             #add 1 to the password strength
-
+            strength += 1
         #otherwise
-
+        else:
             #say that they have to add an number
-
+            print("You should add a number. ")
     #display the password strength
-
+    print(f"Your password strength is {strength}. ")
 
