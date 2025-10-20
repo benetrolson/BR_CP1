a89327e771236944bd1807b311409b72cdeb9644
benetrolson <bennett.rolfson@ucas-edu.net>
2025-10-20 10:51:14 -0600

    functions notes

---
+++ computer_programming/notes/functions.py
@@ -0,0 +1,14 @@
+# BHR 2nd Functions Notes
+
+def add(x,y):
+    return x+y
+
+while num < add(5,5):
+    print("Duck")
+    num+=1
+print("Goose")
+print(f"Results is: {add(-6519146,36516)}")
+total = add(-6519146,36516)
+add(689461,654894)
+print(add(3.14,.85)
+print(add(42,7)
