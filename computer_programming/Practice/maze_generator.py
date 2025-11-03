5f1413b22bd2dc03c70e1411e30093d82d3deb9c
benetrolson <bennett.rolfson@ucas-edu.net>
2025-11-03 10:52:23 -0700

    hard, must make it possible to check if the row and column are the first or last

--- computer_programming/Practice/maze_generator.py
+++ computer_programming/Practice/maze_generator.py
@@ -4,20 +4,62 @@ import random
 bob = turtle.Turtle()
 a = random.randint(1,2)
 b = 3
-maze_grid = [[a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, b]]
-def maze_maker(maze_grid, bob):
+rows = [[a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a]]
+columns = [[a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a], [a, a, a, a, a, a, a, a]]
+
+def randomizer(rows, columns):
+    for row in rows:
+        if row != rows.
+        for piece in row:
+            piece = random.randint(0, 1)
+            row.pop()
+            row.append(piece)
+        rows.pop()
+        row.append(row)
+    for column in columns:
+        for piece in column:
+            piece = random.randint(0, 1)
+            column.pop()
+            column.append(piece)
+        columns.pop()
+        columns.append(column)
+    return(rows, columns)
+
+def maze_checker():
+    while True:
+        rows, columns = randomizer(rows, columns)
+        row_size = len(rows) - 1
+        column_size = len(rows) - 1
+        visited = []
+        stack = [(0, 0)]
+        while stack:
+            x, y = stack.pop()
+            if x == row_size - 1 and y == column_size - 1:
+                return rows, columns
+            if (x, y) in visited:
+                continue
+            visited.add(x, y)
+            if x < row_size -1 and columns[y][x+1] == 0:
+                stack.append(x+1, y)
+            if y < column_size -1 and rows[x][y+1] == 0:
+                stack.append(x, y+1)
+            if x > 0 and columns[y][x+1] == 0:
+                stack.append(x-1, y)
+            if y > 0 and rows[x][y+1] == 0:
+                stack.append(x, y-1)
+
+def maze_maker(rows, columns):
     x = 1
-    for maze_part in maze_grid:
-        for maze_piece in maze_part:
-            maze_piece = random.randint(1,2)
-            if maze_piece == 1:
+    for row in rows:
+        for piece in row:
+            if piece == 1:
                 turtle.pendown()
                 turtle.forward(10)
             else:
                 turtle.penup()
                 turtle.forward(10)
         turtle.pendown()
-        if maze_part == maze_grid[-1]:
+        if row == rows[-1]:
             x = 0
         if x == 1:
             turtle.left(90)
@@ -29,6 +71,6 @@ def maze_maker(maze_grid, bob):
             turtle.forward(10)
             turtle.right(90)
             x -= 1
-maze_maker(maze_grid, bob)
+maze_maker(rows, columns)
 turtle.done()
 
