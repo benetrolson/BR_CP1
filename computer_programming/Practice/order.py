2cfaf034a876a308ab2e7084c99313f7355ea6be
brolfson <bennett.rolfson@ucas-edu.net>
2025-11-10 12:45:29 -0700

    Add menu items for mains, sides, and drinks

--- computer_programming/Practice/order.py
+++ computer_programming/Practice/order.py
@@ -1,11 +1,25 @@
 #BHR 2nd order up
 
 menu = {
-    "worm in an apple": {
-        "alive": 15, 
-        "fried": 12, 
-        "deep fried": 15,
-        "roasted": 10
+    "mains": {
+        "chocolate ice cream": 5,
+        "vanilla ice cream": 5,
+        "strawberry ice cream": 10,
+        "neapolitan ice cream": 10,
+        "vomit ice cream": 50
+    }, 
+    "sides": {
+        "french fries": 1, 
+        "potato salad": 2,
+        "fried crickets": 2, #I've had these before, so you can't say that this is not a feasible food. 
+        "chocolate chips": 3,
+        "cake": 10
+    },
+    "drinks": {
+        "water from the Amazon river": 50, 
+        "water from the Indian Ocean": 50,
+        "water filtered from the blood of human donors": 500,
+        "sludge from nuclear facilities": 5
     }
 }
 
